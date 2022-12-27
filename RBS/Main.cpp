eb51f691413fca1cfbae9fc31563cadba50d857d
Nemanja Mehovic <nemanjamehovic@gmail.com>
2022-12-27 15:25:50 +0100

    ctf example added

--- RBS/Main.cpp
+++ RBS/Main.cpp
@@ -1,5 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "crow.h"
 #include "crow/middlewares/cors.h"
+#include<iostream>
+#include<string>
+
+typedef struct test {
+    std::string userName = "Admin";
+    char checkPassword[20];
+    char password[20] = "Sifra12345";
+} adminData;
+
+adminData testAdmin;
 
 int main()
 {
@@ -9,6 +20,29 @@ int main()
     CROW_ROUTE(app, "/")([]() {
         return "Hello world";
         });
+
+    CROW_ROUTE(app, "/ctf1")
+        .methods("POST"_method)
+        ([](const crow::request& req) {
+            auto x = crow::json::load(req.body);
+
+            if (!x || !x.has("username") || !x.has("password"))
+                return crow::response(400);
+
+            if (testAdmin.userName.compare(std::string(x["username"].s())) == 0)
+            {
+                strcpy(testAdmin.checkPassword, std::string(x["password"].s()).c_str());
+
+                std::cout << testAdmin.password << std::endl;
+
+                if (strcmp(testAdmin.checkPassword, testAdmin.password) != 0)
+                    return crow::response(401);
+            }
+            else
+                return crow::response(400);
+
+            return crow::response(200);
+            });
 
     app.port(18080).multithreaded().run();
 }
